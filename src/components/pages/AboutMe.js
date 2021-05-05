3b2eb36e8897ba4fba314a48bc8e5fe808acea5f
yilin <yilinruan@gmail.com>
2021-05-05 03:22:25 -0700

    fix home page mobile sizing bug

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -3,16 +3,8 @@ import React from 'react'
 export default function AboutMe() {
 
     return (
-        <div className="AboutMe">
-            <div className="AboutMe-Header">
-                <h3 className="AboutMe-Header-Title">
-                    HI visitor, my name is
-                </h3>
-                <h2 className="AboutMe-Header-Name">YILIN RUAN</h2>
-                
-            </div>
+        <div className="aboutme">
 
-          
           
         </div>
     )
